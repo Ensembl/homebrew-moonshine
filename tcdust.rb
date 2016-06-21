c1369da77932468e7d28b7b0ff26442b9982c4f5
Andrew Yates <ayates@ebi.ac.uk>
2016-06-21 17:40:07 +0100

    Switching to using github private repos

--- tcdust.rb
+++ tcdust.rb
@@ -1,10 +1,10 @@
 class Tcdust < Formula
 
   desc "Finds low complexity repeats with more efficient IO"
-  url "http://example.org"
 
-  url "file:///nfs/gns/homes/ayates/linuxbrew/private_source/tcdust-1.0.0.tar.gz"
-  sha256 "5e5b02e80415c5916adbd05c5872fce6508a712f909cc090bcd71ac3804ab6d0"
+  url 'git@github.com:Ensembl/tcdust.git', :using => :git, :revision => 'master'
+  #url "file:///nfs/gns/homes/ayates/linuxbrew/private_source/tcdust-1.0.0.tar.gz"
+  #sha256 "5e5b02e80415c5916adbd05c5872fce6508a712f909cc090bcd71ac3804ab6d0"
   version "1.0.0"
 
   def install 

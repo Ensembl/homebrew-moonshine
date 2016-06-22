ea8b4891d84a15cd4d122d2876e30a255ccf3312
Andrew Yates <ayates@ebi.ac.uk>
2016-06-22 15:23:32 +0100

    Fixing with a patch rather than inline replace

--- pmatch.rb
+++ pmatch.rb
@@ -9,12 +9,9 @@ class Pmatch < Formula
   #sha256 "3ccabf799b0c04f01fed86e0c00a872cc633a02bbf696bf9df6ebf7f77b8397d"
   version "1.0.0"
 
+  patch :DATA
+
   def install
-    inreplace 'Makefile', '#CC=g', 'CC=g'
-    inreplace 'Makefile', '#CFLAGS=-O6', 'CFLAGS=-O6'
-    inreplace 'Makefile', 'CC=cc', ''
-    inreplace 'Makefile', 'CFLAGS=-fast', '#CFLAGS=-fast'
-    inreplace 'Makefile', "glib-config", "pkg-config glib-2.0"
     system 'make'
     bin.install 'pmatch'
   end
@@ -23,3 +20,43 @@ class Pmatch < Formula
     system 'pmatch'
   end
 end
+__END__
+diff --git a/Makefile b/Makefile
+index f72b61b..1c0a457 100644
+--- a/Makefile
++++ b/Makefile
+@@ -2,11 +2,11 @@
+ # CVS info: $Id: Makefile,v 1.4 2001/10/26 09:35:02 searle Exp $
+ 
+ # For gcc
+-#CC=gcc
+-#CFLAGS=-O6 -fomit-frame-pointer -finline-functions
++CC=gcc
++CFLAGS=-O6 -fomit-frame-pointer -finline-functions
+ # For Alpha compiler
+-CC=cc
+-CFLAGS=-fast -O4 -arch ev67
++#CC=cc
++#CFLAGS=-fast -O4 -arch ev67
+ 
+ all: composition seqsample pmatch seqpress
+ 
+@@ -22,7 +22,7 @@ peptide.o: peptide.c peptide.h
+ 	$(CC) -c $(CFLAGS) peptide.c
+ 
+ composition: composition.c readseq.o
+-	$(CC) -g -o $(@) -lm composition.c readseq.o
++	$(CC) -g -o $(@) composition.c readseq.o -lm
+ 
+ dbshatter: dbshatter.c readseq.o
+ 	$(CC) -g -o $(@) dbshatter.c readseq.o
+@@ -31,7 +31,7 @@ seqsample: seqsample.c readseq.o
+ 	$(CC) -g -o $(@) seqsample.c readseq.o
+ 
+ pmatch: pmatch.c readseq.o peptide.o
+-	$(CC) $(CFLAGS) -o $@ `glib-config --cflags` pmatch.c readseq.o peptide.o `glib-config --libs`
++	$(CC) $(CFLAGS) -o $@ `pkg-config glib-2.0 --cflags` pmatch.c readseq.o peptide.o `pkg-config glib-2.0 --libs`
+ 
+ seqpress: seqpress.c readseq.o
+ 	$(CC) -g -o $(@) seqpress.c readseq.o
+

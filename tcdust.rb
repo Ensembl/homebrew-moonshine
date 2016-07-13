0273b4839640642e85b0bc648c6e62bed4784599
Andrew Yates <andrewyatz@users.noreply.github.com>
2016-07-13 16:14:40 +0100

    Switch to jENSEMBL_MOONSHINE_ARCHIVE for source

--- tcdust.rb
+++ tcdust.rb
@@ -1,10 +1,9 @@
 class Tcdust < Formula
 
   desc "Finds low complexity repeats with more efficient IO"
-
-  url 'git@github.com:Ensembl/tcdust.git', :using => :git, :revision => 'master'
-  #url "file:///nfs/gns/homes/ayates/linuxbrew/private_source/tcdust-1.0.0.tar.gz"
-  #sha256 "5e5b02e80415c5916adbd05c5872fce6508a712f909cc090bcd71ac3804ab6d0"
+  odie 'No ENSEMBL_MOONSHINE_ARCHIVE environment variable found' if ENV['ENSEMBL_MOONSHINE_ARCHIVE'].nil?
+  url ENV['ENSEMBL_MOONSHINE_ARCHIVE']+"/tcdust-1.0.0.tar.gz"
+  sha256 "5e5b02e80415c5916adbd05c5872fce6508a712f909cc090bcd71ac3804ab6d0"
   version "1.0.0"
 
   def install 

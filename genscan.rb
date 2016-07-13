7c054b8573a515a5cd3a60ceb0b5b356381c37ea
Andrew Yates <ayates@ebi.ac.uk>
2016-07-13 17:46:45 +0100

    Adding genscan

---
+++ genscan.rb
@@ -0,0 +1,26 @@
+class Genscan < Formula
+
+  # When installing you must download the appropriate archive which is 
+  # uuencoded and issue the commands (in the ENSEMBL_MOONSHINE_ARCHIVE dir):
+  # 
+  # uudecode uudecode genscanlinux.tar.uue
+  # gzip genscanlinux.tar
+  # mv genscanlinux.tar.gz genscan-1.0.0.tar.gz
+
+  desc "General-purpose gene identification program"
+  homepage "http://genes.mit.edu/gendist.html"
+  odie 'No ENSEMBL_MOONSHINE_ARCHIVE environment variable found' if ENV['ENSEMBL_MOONSHINE_ARCHIVE'].nil?
+  url 'file://'+ENV['ENSEMBL_MOONSHINE_ARCHIVE']+"/genscan-1.0.0.tar.gz"
+  sha256 "b71fc3233555a7928a369885a8ad867f3db2a4e0e38eda223ca8f4654801a687"
+  version "1.0.0"
+
+  def install 
+    bin.install 'genscan'
+    share.install Dir['*.smat']
+    doc.install 'README'
+  end
+
+  test do
+    system "#{bin}/genscan"
+  end
+end

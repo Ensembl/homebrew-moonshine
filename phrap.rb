47c55137f3d9b57554c11eafefd21d5ce10506e6
Thibaut Hourlier <thibaut@ebi.ac.uk>
2016-11-07 13:15:49 +0000

    Removed ensembl/ensembl/repeatmasker as dependency, phrap is now a dependency of ensembl/ensembl/repeatmasker

--- phrap.rb
+++ phrap.rb
@@ -7,8 +7,6 @@ class Phrap < Formula
   sha256 "81f50c4410e8604cdefcc34ef6dc7b037be3bb45b94c439611a5590c1cf83665"
   version "1.090518"
 
-  depends_on "ensembl/ensembl/repeatmasker" => :recommended
-
   def install
     system "make"
     doc.install('phrap.doc')
@@ -30,36 +28,6 @@ class Phrap < Formula
     bin.install('loco')
     bin.install('calf_merge')
    end
-
-  def post_install
-    if build.with?("repeatmasker")
-      (Formula["ensembl/ensembl/repeatmasker"].opt_libexec/"cross_match_config.txt").write <<-EOS.undent
-
-        env
-        #{Formula["ensembl/ensembl/repeatmasker"].opt_libexec}
-        #{HOMEBREW_PREFIX}/bin/trf
-        1
-        #{HOMEBREW_PREFIX}/bin
-        Y
-        2
-        #{HOMEBREW_PREFIX}/bin
-        N
-        4
-        #{HOMEBREW_PREFIX}/bin
-        N
-        5
-        EOS
-      system "cd #{Formula['ensembl/ensembl/repeatmasker'].opt_libexec} && ./configure <cross_match_config.txt"
-    end
-  end
-
-  def caveats; <<-EOS.undent
-    Unless you used --without-repeatmasker RepeatMasker has
-    been configured to use cross_match as default search engine.
-    You may reconfigure RepeatMasker by running
-      cd #{Formula['ensembl/ensembl/repeatmasker'].opt_libexec} && ./configure
-    EOS
-  end
 
   test do
     system "cross_match", "vector.seq"

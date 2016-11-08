c196e0c77e5ed68a8575f896419ee2e55f147054
Thibaut Hourlier <thibaut@ebi.ac.uk>
2016-11-08 10:14:04 +0000

    Removed dependency on repeatmasker

--- repbase.rb
+++ repbase.rb
@@ -7,22 +7,8 @@ class Repbase < Formula
   sha256 "d03ee3ca07a0c95764be05e9ba8c83203f82cb13e87874a735e80b89447c7919"
   version "20150807"
 
-  depends_on "ensembl/ensembl/repeatmasker" => :recommended
-
   def install
     libexec.install Dir["./*"]
-    if build.with?("repeatmasker")
-      (Formula["ensembl/ensembl/repeatmasker"].libexec/"Libraries").install_symlink libexec/"RepeatAnnotationData.pm"
-      (Formula["ensembl/ensembl/repeatmasker"].libexec/"Libraries").install_symlink libexec/"RepeatMaskerLib.embl"
-      (Formula["ensembl/ensembl/repeatmasker"].libexec/"Libraries").install_symlink libexec/"taxonomy.dat"
-    end
-   end
-
-  def caveats; <<-EOS.undent
-    Unless you used --without-repeatmasker RepeatMasker has
-    been configured to use RepBase as default repeats database.
-    You will need to reinstall repeatmasker to avoid using RepBase
-    EOS
   end
 
 end

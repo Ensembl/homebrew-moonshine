553b7414f31c020f0ab4f32c91af9291ca66c70b
Thibaut Hourlier <thibaut@ebi.ac.uk>
2016-11-17 14:40:26 +0000

    New repbase version

--- repbase.rb
+++ repbase.rb
@@ -3,9 +3,9 @@ class Repbase < Formula
   desc "Curated set of repeat libraries for RepeatMasker"
   homepage "http://www.girinst.org/repbase/index.html"
   odie 'No ENSEMBL_MOONSHINE_ARCHIVE environment variable found' if ENV['ENSEMBL_MOONSHINE_ARCHIVE'].nil?
-  url 'file://'+ENV['ENSEMBL_MOONSHINE_ARCHIVE']+"/repeatmaskerlibraries-20150807.tar.gz"
-  sha256 "d03ee3ca07a0c95764be05e9ba8c83203f82cb13e87874a735e80b89447c7919"
-  version "20150807"
+  url 'file://'+ENV['ENSEMBL_MOONSHINE_ARCHIVE']+"/repeatmaskerlibraries-20160829.tar.gz"
+  sha256 "91d29e03302d4ba553a5236c59e78eeccbc53908922f25ef30b240c4e664abb1"
+  version "20160829"
 
   def install
     libexec.install Dir["./*"]
